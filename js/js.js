e943998255fc20e59dde1bcff3ab1c8d2a5799a8
TRI VI <trivi2710@gmail.com>
2017-07-31 15:38:24 -0400

    update

---
+++ js/js.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){	
+  var typed3 = new Typed('#typedText', {
+    strings: ['Welcome to my page!', 'I am <strong>Tri Vi</strong> ...', 'A <strong>Fullstack Engineer </strong> ...','<strong> A Software Engineer</strong> ...', 'Check out my projects!'],
+    typeSpeed: 60,
+    backSpeed: 60,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+})
